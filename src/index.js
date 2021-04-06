923ba27f0f9a409c664a6fd0465357cb340fce5a
Muhammad Shoaib <yourgiteiamshoaibktk@gmail.com>
2021-04-07 00:26:40 +0500

    Search functionality added

--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import App  from './App';
 
-import  { robots } from './robots';
-import CardList  from './CardList';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <CardList robots={robots} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
